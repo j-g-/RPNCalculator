d2ae27910a259a2d89b594d56e8c8d8ccbcab979
J. García <u0x004a@gmail.com>
2018-09-21 04:28:47 -0600

    Arguments parser

--- src/cli/CLI.java
+++ src/cli/CLI.java
@@ -6,6 +6,8 @@
 package cli;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import rpncalculator.RPNCalculator;
 
 /**
@@ -14,27 +16,69 @@ import rpncalculator.RPNCalculator;
  */
 public class CLI {
 	static RPNCalculator rpnc  = RPNCalculator.getInstance();
-	private static void interactiveMode() {
+	public static boolean showHelp = false;
+	public static boolean showHistory = false;
+	public static String historyMode = "normal";
+	public static boolean parseFile = false;
+	public static boolean interactiveMode = false;
+	public static String fileToParse = "normal";
+
+	private static void startInteractiveMode() {
 		String prompt = ">>> ";
 		Scanner sc = new Scanner(System.in);
 	    for(;;){
 			rpnc.setInput("stdin");
 			System.out.print(prompt);
 			String line = sc.nextLine();
-			switch (line) {
-				case "help":
+			if (line.matches("h|help")){
 					printHelp();
-					break;
-				case "exit":
-				case "quit":
+			} else if (line.matches("exit|quit|q")){
 					System.exit(0);
-					break;
-				case "history":
+			} else if (line.matches("history.*|hist.*")){
+				//Matcher m = 	
+				Pattern p =  Pattern.compile("(?:^.*)(history|hist)(?:\\s*)(?<opt>r.*|d.*)*(?:$)");
+				Matcher m = p.matcher(line);
+				String opt = "";
+				if (m.matches()){
+					opt = m.group("opt");
+				}
+				if (opt == null ){
+					opt = "";
+				}
+				if (opt.matches("r.*")){
+					rpnc.getCalculationsHistory().printOrderedByResult(true);
+				} else if (opt.matches("d.*")){
 					rpnc.getCalculationsHistory().printOrderedByDate();
-					break;
-				default:
+				} else {
+					rpnc.getCalculationsHistory().printOrderedByResult(false);
+				}
+			} else {
 					System.out.println(rpnc.parseLine(line));
 			}
+		}
+	}
+
+	public static void parseArgs(String [] args){
+		if (args.length >  0){
+			for(String arg : args) {
+				if(arg.matches("-h|--help")) {
+					showHelp = true;
+				} else if(arg.matches("--history|-ho|--history=ord.*")) {
+					showHistory = true;
+					historyMode = "o";
+				} else if(arg.matches("-hr|--history=rev.*")) {
+					showHistory = true;
+					historyMode = "r";
+				} else if(arg.matches("-hd|--history=da.*")) {
+					showHistory = true;
+					historyMode = "d";
+				} else {
+					 fileToParse = arg;
+					 parseFile   = true;
+				}
+			}
+		} else {
+			interactiveMode = true;
 		}
 	}
 
@@ -42,17 +86,41 @@ public class CLI {
 		String[][] commands = {
 			{"help", "Prints this help."},
 			{"quit/exit", "Exit program."},
-			{"history", "Show history."}
+			{"history [r|d]", "Show history. r = reverse result, d = date"}
+		};
+		String[][] args = {
+			{"-h --help", "Print this help."},
+			{"-ho", ""},
+			{"--history=ordered", "Print sorted history results."},
+			{"-hr", ""},
+			{"--history=reverse", "Print sorted history results in reverse."},
+			{"-hd", ""},
+			{"--history=date", "Print sorted history results by date."},
+			{"[file]", "File to parse"}
 		};
 		String help = "";
 		help += "********************************************************************************" + System.lineSeparator();
-		help += "Commands" + System.lineSeparator();
+		help += "Arguments" + System.lineSeparator();
+		help += "********************************************************************************" + System.lineSeparator();
+		help += "\t java -jar RPNCalculator.jar [[options]|[file]] "+ System.lineSeparator();
+		help += "Options:"+ System.lineSeparator();
+		for (String []arg : args){
+			help += String.format("\t%-24s%s",arg[0], arg[1]) + 
+					System.lineSeparator();
+		}
+
+		help += System.lineSeparator();
+		help += System.lineSeparator();
+		help += "********************************************************************************" + System.lineSeparator();
+		help += "Interactive mode commands" + System.lineSeparator();
 		help += "********************************************************************************" + System.lineSeparator();
 
 		for (String []command : commands){
 			help += String.format("\t%-16s%s",command[0], command[1]) + 
 					System.lineSeparator();
 		}
+		help += System.lineSeparator();
+		help += System.lineSeparator();
 		help += "********************************************************************************" + System.lineSeparator();
 		help += "Expresions" + System.lineSeparator();
 		help += "********************************************************************************" + System.lineSeparator();
@@ -71,18 +139,31 @@ public class CLI {
 	}
 
 	public static void main(String[] args) {
-		if (args.length == 1){
-			switch (args[0]) {
-				case "-h":
-				case "--help":
-				case "help":
-					printHelp();
-					break;
-				default:
-					rpnc.processFile(args[0]);
+		parseArgs(args);
+		if (interactiveMode){
+			startInteractiveMode();
+		} else {
+			if (showHelp){
+				printHelp();
 			}
-		} else if (args.length == 0){
-			interactiveMode();
+			if (showHistory) {
+				switch (historyMode) {
+					case "o":
+						rpnc.getCalculationsHistory().printOrderedByResult(false);
+						break;
+					case "r":
+						rpnc.getCalculationsHistory().printOrderedByResult(true);
+						break;
+					case "d":
+						rpnc.getCalculationsHistory().printOrderedByDate();
+						break;
+					default:
+						rpnc.getCalculationsHistory().printOrderedByDate();
+				}
+			}
+			if(parseFile){
+				rpnc.processFile(fileToParse);
+			}
 		}
 	}
 
